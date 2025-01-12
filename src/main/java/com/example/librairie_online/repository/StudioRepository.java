b580ff000087a0365e3ab98971838439bc685924
c-cedina <cdc.cedina@gmail.com>
2025-01-12 15:13:04 +0100

    Added Studio repository

---
+++ src/main/java/com/example/librairie_online/repository/StudioRepository.java
@@ -0,0 +1,9 @@
+package com.example.librairie_online.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.librairie_online.entity.Studio;
+
+public interface StudioRepository extends JpaRepository<Studio, String> {
+
+}
