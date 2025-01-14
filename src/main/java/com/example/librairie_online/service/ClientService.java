9d8e2957bbef65dda9bc00837186e16873c62c9c
c-cedina <cdc.cedina@gmail.com>
2025-01-14 08:47:49 +0100

    Added Crud

--- src/main/java/com/example/librairie_online/service/ClientService.java
+++ src/main/java/com/example/librairie_online/service/ClientService.java
@@ -1,8 +1,50 @@
 package com.example.librairie_online.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.example.librairie_online.entity.Client;
 import com.example.librairie_online.repository.ClientRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 
 public class ClientService {
     private ClientRepository clientRepository;
+
+    // Create
+    public void create(Client client) {
+        this.clientRepository.save(client);
+    }
+
+    // Read
+    public List<Client> read() {
+        return this.clientRepository.findAll();
+    }
+
+    public Client read(int id) {
+        Optional<Client> optionalClient = this.clientRepository.findById(id);
+        if (optionalClient.isPresent()) {
+            return optionalClient.get();
+        }
+        return null;
+    }
+
+    // Update
+    public void update(int id, Client client) {
+        Client dbClient = this.read(id);
+        dbClient.setAge(client.getAge());
+        dbClient.setNom(client.getNom());
+        dbClient.setDate_naissance(client.getDate_naissance());
+        dbClient.setPrenom(client.getPrenom());
+        dbClient.setSexe(null);
+
+    }
+
+    // delete
+    public void delete(int id) {
+        this.clientRepository.deleteById(id);
+    }
 
 }
