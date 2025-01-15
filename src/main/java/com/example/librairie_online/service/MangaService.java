76fbe75d295b3f635ebcf3cccafa93c5c5a6fe8b
c-cedina <cdc.cedina@gmail.com>
2025-01-15 17:47:41 +0100

    Added complet crud in Manga service

--- src/main/java/com/example/librairie_online/service/MangaService.java
+++ src/main/java/com/example/librairie_online/service/MangaService.java
@@ -1,6 +1,7 @@
 package com.example.librairie_online.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,10 @@ import com.example.librairie_online.repository.MangaRepository;
 public class MangaService {
     private MangaRepository mangaRepository;
 
+    public void Create(Manga manga) {
+        this.mangaRepository.save(manga);
+    }
+
     public MangaService(MangaRepository mangaRepository) {
         this.mangaRepository = mangaRepository;
     }
@@ -19,4 +24,26 @@ public class MangaService {
         return this.mangaRepository.findAll();
     }
 
+    public Manga readByID(int id) {
+        Optional<Manga> optionalManga = this.mangaRepository.findById(id);
+        if (optionalManga.isPresent()) {
+            return optionalManga.get();
+        }
+        return null;
+    }
+
+    public void update(int id, Manga manga) {
+        Manga dbManga = readByID(id);
+        dbManga.setDate_parution(manga.getDate_parution());
+        dbManga.setFournisseur(manga.getFournisseur());
+        dbManga.setMangaka(manga.getMangaka());
+        dbManga.setNbExemplaire(manga.getNbExemplaire());
+        dbManga.setNom(manga.getNom());
+        dbManga.setNserie(manga.getNserie());
+        dbManga.setTome(manga.getTome());
+    }
+
+    public void delete(int id) {
+        this.mangaRepository.deleteById(id);
+    }
 }
