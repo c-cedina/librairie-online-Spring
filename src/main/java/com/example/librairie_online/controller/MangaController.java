484b10068235cb96edc7797d1eefbd6c7735e402
c-cedina <cdc.cedina@gmail.com>
2025-01-18 03:27:47 +0100

    added read by id Manga controller

--- src/main/java/com/example/librairie_online/controller/MangaController.java
+++ src/main/java/com/example/librairie_online/controller/MangaController.java
@@ -6,10 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
 import com.example.librairie_online.entity.Manga;
 
 import com.example.librairie_online.service.MangaService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping(path = "Manga")
@@ -25,4 +29,16 @@ public class MangaController {
     public List<Manga> read() {
         return this.mangakaService.read();
     }
+
+    @GetMapping("id")
+    public ResponseEntity<Manga> readById(@RequestParam int id) {
+        Manga manga = this.mangakaService.readByID(id);
+        if (manga != null) {
+            return new ResponseEntity<>(manga, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
 }
