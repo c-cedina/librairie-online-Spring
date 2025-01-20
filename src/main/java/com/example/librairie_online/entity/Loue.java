9a5158510e4cb7772654587dfd9af91990df2737
c-cedina <cdc.cedina@gmail.com>
2025-01-20 14:15:35 +0100

    Added Loue entity

---
+++ src/main/java/com/example/librairie_online/entity/Loue.java
@@ -0,0 +1,36 @@
+package com.example.librairie_online.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+public class Loue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "nAdherent")
+    private Client client;
+
+    @ManyToOne
+    @JoinColumn(name = "nSerie")
+    private Manga manga;
+
+    private LocalDate dateDebut;
+    private LocalDate dateFin;
+}
---
+++ src/main/java/com/example/librairie_online/repository/LoueRepository.java
@@ -0,0 +1,9 @@
+package com.example.librairie_online.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.librairie_online.entity.Loue;
+
+public interface LoueRepository extends JpaRepository<Loue, Integer> {
+
+}
